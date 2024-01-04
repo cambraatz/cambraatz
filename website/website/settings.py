a305312b0d26e8217f1db4cad6cc679036ad7cfe
root <root@server.tcsservices.com>
2024-01-04 13:22:43 -0600

    commit following pysqlite3-binary resolution

--- website/website/settings.py
+++ website/website/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
